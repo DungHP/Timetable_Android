6ebeff98efa9833b862bdf846ad7e4032d572d85
nddung1508@gmail.com <nguyenducdung15082002@gmail.com>
2022-09-13 20:54:25 +0700

    Bind Timetable Activity, Add Create Button to TimetableActivity Layout

--- app/src/main/java/com/example/timetable_android/TimetableActivity.kt
+++ app/src/main/java/com/example/timetable_android/TimetableActivity.kt
@@ -2,10 +2,14 @@ package com.example.timetable_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.timetable_android.databinding.ActivityTimetableBinding
 
 class TimetableActivity : AppCompatActivity() {
+    private var binding: ActivityTimetableBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_timetable)
+        binding = ActivityTimetableBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
     }
 }
